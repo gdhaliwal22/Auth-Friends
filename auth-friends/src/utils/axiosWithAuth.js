8fdeb2efae243092fb6e2f3ce665ddf1a042bf1d
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2019-11-12 17:30:23 -0800

    MVP completed

--- auth-friends/src/utils/axiosWithAuth.js
+++ auth-friends/src/utils/axiosWithAuth.js
@@ -1,14 +1,11 @@
 import axios from 'axios';
 
-
-
-const axiosWithAuth = () => {
-    const token = localStorage.getItem('token');
-
-    return axios.create({
-        baseURL:'http://localhost:5000/api',
-        headers: {
-            Authorization: token
-        }
-    })
+export const axiosWithAuth = () => {
+  const token = localStorage.getItem('token');
+  return axios.create({
+    baseURL:'http://localhost:5000/api',
+    headers: {
+      Authorization: token
+    }
+  })
 }
